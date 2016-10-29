2d9a002876e986b084c42cbedf3d0f27229ebed7
michaelmay <maydev99@gmail.com>
2016-10-29 18:27:59 -0400

    Setup Complete

--- app/src/main/java/com/droidloft/myalert/MainActivity.java
+++ app/src/main/java/com/droidloft/myalert/MainActivity.java
@@ -1,13 +1,31 @@
 package com.droidloft.myalert;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button alertButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        alertButton = (Button) findViewById(R.id.alertButton);
+
+        alertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
